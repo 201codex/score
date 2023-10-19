763a9cc932d2ad291d268d7f6594714ff14729b0
Olajide Joseph <72675871+201codex@users.noreply.github.com>
2023-10-19 13:03:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+awaySCORE = document.getElementById('away-score');
+count = 0;
+function increase1() {
+    count += 1;
+    awaySCORE.textContent = count;
+}
+awaySCORE = document.getElementById('away-score');
+count = 0;
+function increase2() {
+    count += 2;
+    awaySCORE.textContent = count;
+}
+awaySCORE = document.getElementById('away-score');
+count = 0;
+function increase3() {
+    count += 3;
+    awaySCORE.textContent = count;
+}
+homeSCORE = document.getElementById('home-score');
+score = 0;
+function increasehome1() {
+    score += 1;
+    homeSCORE.textContent = score;
+}
+homeSCORE = document.getElementById('home-score');
+score = 0;
+function increasehome2() {
+    score += 2;
+    homeSCORE.textContent = score;
+}
+homeSCORE = document.getElementById('home-score');
+score = 0;
+function increasehome3() {
+    score += 3;
+    homeSCORE.textContent = score;
+}
